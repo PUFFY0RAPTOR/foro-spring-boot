740e3c4744708146af53bc88d4083e3e004da9e2
SebasPertuz <sebastianpertuzg@hotmail.com>
2023-03-03 10:41:48 -0500

    Añadiendo dependencias para generar PDF

--- ForoJAVA/src/main/java/com/foro/juniors/foro/Controllers/Reporte.java
+++ ForoJAVA/src/main/java/com/foro/juniors/foro/Controllers/Reporte.java
@@ -1,16 +1,25 @@
 package com.foro.juniors.foro.Controllers;
 
+import com.foro.juniors.foro.Models.Service.*;
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.HtmlConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
 
-import com.foro.juniors.foro.Models.Service.IComentarioService;
-import com.foro.juniors.foro.Models.Service.IPublicacionService;
-import com.foro.juniors.foro.Models.Service.ITemaService;
-import com.foro.juniors.foro.Models.Service.IUsuarioService;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("reportes")
@@ -25,10 +34,10 @@ public class Reporte {
     private IPublicacionService publicacionService;
 
     @Autowired
-    private ITemaService temaService;
+    private IComentarioService comentarioService;
 
     @Autowired
-    private IComentarioService comentarioService;
+    private IRespuestaService respuestaService;
 
 
     @GetMapping("/generar")
@@ -40,8 +49,36 @@ public class Reporte {
         model.addAttribute("titulo", "Reportes de usuarios");
         model.addAttribute("usuario", usuarioService.findAll());
         model.addAttribute("publicacion", publicacionService.findAll());
+        model.addAttribute("comentario", comentarioService.findAll());
+        model.addAttribute("respuesta", respuestaService.findAll());
         
         return "reportes";
+    }
+
+    @RequestMapping(path = "/pdf")
+    public ResponseEntity<?> getPDF(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        TemplateEngine templateEngine = new TemplateEngine();
+
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        context.setVariable("usuario", usuarioService.findAll());
+        context.setVariable("publicacion", publicacionService.findAll());
+        context.setVariable("comentario", comentarioService.findAll());
+        context.setVariable("respuesta", respuestaService.findAll());
+        String contentHtml = templateEngine.process("reportes", context);
+
+        ByteArrayOutputStream target = new ByteArrayOutputStream();
+
+        ConverterProperties converterProperties = new ConverterProperties();
+        converterProperties.setBaseUri("http://localhost:8080");
+
+        HtmlConverter.convertToPdf(contentHtml, target, converterProperties);
+
+        byte[] bytes = target.toByteArray();
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(bytes);
     }
 
 }
